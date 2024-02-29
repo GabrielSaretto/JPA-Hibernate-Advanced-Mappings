ffc2711d9a594e603c424ffb7d8dc6b354b2324b
GabrielSaretto <gabriel.saretto2@gmail.com>
2024-02-29 16:42:13 -0300

    Update DAO and Main App

--- src/main/java/com/saretto/cruddemo/CruddemoApplication.java
+++ src/main/java/com/saretto/cruddemo/CruddemoApplication.java
@@ -4,6 +4,7 @@ import com.saretto.cruddemo.dao.AppDAO;
 import com.saretto.cruddemo.entity.Course;
 import com.saretto.cruddemo.entity.Instructor;
 import com.saretto.cruddemo.entity.InstructorDetail;
+import com.saretto.cruddemo.entity.Review;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,7 +25,27 @@ public class CruddemoApplication {
 
 		return runner -> {
 
+			createCourseAndReviews(appDAO);
 		};
+	}
+
+	private void createCourseAndReviews(AppDAO appDAO) {
+
+		// create a course
+		Course tempCourse = new Course("Pacman - How To Score One Million Points");
+
+		// add some reviews
+		tempCourse.addReview(new Review("Great course ... loved it!"));
+		tempCourse.addReview(new Review("Cool course, job well done."));
+		tempCourse.addReview(new Review("What a dumb course, you are an idiot!"));
+
+		// save the course ... and leverage the cascade all
+		System.out.println("Saving the course");
+		System.out.println(tempCourse);
+		System.out.println(tempCourse.getReviews());
+
+		appDAO.save(tempCourse);
+
 	}
 
 	private void deleteCourse(AppDAO appDAO) {
--- src/main/java/com/saretto/cruddemo/entity/Review.java
+++ src/main/java/com/saretto/cruddemo/entity/Review.java
@@ -17,7 +17,7 @@ public class Review {
     // annotate fileds
 
     @Id
-    @GeneratedValue(GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
 
